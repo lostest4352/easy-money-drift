6aaeddcbdf7074398c48f701bb679780766d34ba
user <username@email.com>
2024-10-16 11:53:03 +0545

    wrote some drift crud function

--- lib/database/drift_database.dart
+++ lib/database/drift_database.dart
@@ -4,7 +4,7 @@ import 'package:path/path.dart' as p;
 import 'package:drift/drift.dart';
 import 'package:drift/native.dart';
 import 'package:flutter/foundation.dart';
-import 'package:flutter_expense_tracker/database/transaction.dart';
+import 'package:flutter_expense_tracker/database/categories.dart';
 import 'package:path_provider/path_provider.dart';
 
 part 'drift_database.g.dart';
@@ -31,7 +31,56 @@ class AppDatabase extends _$AppDatabase {
   @override
   int get schemaVersion => 1;
 
-  //
+  //Transaction related
+  Stream<List<TransactionModelDriftData>> getTransactionData() async* {
+    final allTransactions = (select(transactionModelDrift)
+          ..orderBy([
+            (transaction) => OrderingTerm(
+                expression: transaction.id, mode: OrderingMode.desc)
+          ]))
+        .watch();
+    yield* allTransactions;
+  }
+
+  Stream<List<TransactionModelDriftData>> getTransactionDataByDate(
+      {required String startTime, required String endTime}) async* {
+    final allTransactions = (select(transactionModelDrift)
+          ..where((val) => val.dateAndTime.isBetweenValues(startTime, endTime))
+          ..orderBy([
+            (transaction) => OrderingTerm(
+                expression: transaction.dateAndTime, mode: OrderingMode.desc)
+          ]))
+        .watch();
+    yield* allTransactions;
+  }
+
+  Stream<List<TransactionModelDriftData>> getTransactionDataSearchItem(
+      {required String keyword}) async* {
+    final allTransactions = (select(transactionModelDrift)
+          ..where((val) {
+            // may cause issues
+            return val.categoryModel.contains(keyword);
+          })
+          ..orderBy([
+            (transaction) => OrderingTerm(
+                expression: transaction.id, mode: OrderingMode.desc)
+          ]))
+        .watch();
+    yield* allTransactions;
+  }
+
+  // Category related
+  Stream<List<CategoryModelDriftData>> getCategoryData() async* {
+    final allCategories = (select(categoryModelDrift)
+          ..orderBy([
+            (cateories) => OrderingTerm(
+                expression: cateories.categoryModel, mode: OrderingMode.desc)
+          ]))
+        .watch();
+    yield* allCategories;
+  }
+
+  // TODO may do delete
 }
 
 //
